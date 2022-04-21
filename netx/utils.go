8d95092fff7ffe7e62cd43b7c958c0b8359222d1
Alex Jeannopoulos <alex@jeannopoulos.com>
2022-04-21 17:48:19 -0400

    latest working - .999 release

--- netx/utils.go
+++ netx/utils.go
@@ -1,72 +1,117 @@
 package netx
 
 import (
-	"fmt"
-	"github.com/pkg/errors"
-	"net"
+    "fmt"
+    "github.com/pkg/errors"
+    "net"
+    "os"
 )
 
 // GetFreePort asks the kernel for a free open port that is ready to use.
 func GetFreePort(start, end int) (int, error) {
-	if start < 1 || start > 65535 {
-		return -1, errors.Errorf("invalid start port: %d", start)
-	}
+    if start < 1 || start > 65535 {
+        return -1, errors.Errorf("invalid start port: %d", start)
+    }
 
-	if end < 1 || end > 65535 {
-		return -1, errors.Errorf("invalid end port: %d", end)
-	}
-	if start >= end {
-		return -1, errors.Errorf("invalid start: %d/end: %d  portS", start, end)
-	}
+    if end < 1 || end > 65535 {
+        return -1, errors.Errorf("invalid end port: %d", end)
+    }
+    if start >= end {
+        return -1, errors.Errorf("invalid start: %d/end: %d  portS", start, end)
+    }
 
-	var port = -1
-	for p := start; p < end; p++ {
-		avail, err := IsPortFree(p)
-		if err == nil && avail == true {
-			port = p
-			break
-		}
-	}
-	if port == -1 {
-		return -1, errors.Errorf("unable to find port in range[%d:%d]", start, end)
-	}
-	return port, nil
+    var port = -1
+    for p := start; p < end; p++ {
+        avail, err := IsPortFree(p)
+        if err == nil && avail == true {
+            port = p
+            break
+        }
+    }
+    if port == -1 {
+        return -1, errors.Errorf("unable to find port in range[%d:%d]", start, end)
+    }
+    return port, nil
 }
 
 // IsPortFree asks the kernel is a specific port is free
 func IsPortFree(port int) (bool, error) {
-	if port < 1 || port > 65535 {
-		return false, errors.Errorf("invalid port: %d", port)
-	}
-	listener := fmt.Sprintf(":%d", port)
-	addr, err := net.ResolveTCPAddr("tcp", listener)
-	if err != nil {
-		return false, err
-	}
+    if port < 1 || port > 65535 {
+        return false, errors.Errorf("invalid port: %d", port)
+    }
+    listener := fmt.Sprintf(":%d", port)
+    addr, err := net.ResolveTCPAddr("tcp", listener)
+    if err != nil {
+        return false, err
+    }
 
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		return false, err
-	}
-	defer func() {
-		_ = l.Close()
-	}()
-	return true, nil
+    l, err := net.ListenTCP("tcp", addr)
+    if err != nil {
+        return false, err
+    }
+    defer func() {
+        _ = l.Close()
+    }()
+    return true, nil
 }
 
 // GetRandomFreePort asks the kernel for a free open port that is ready to use.
 func GetRandomFreePort() (int, error) {
-	addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
-	if err != nil {
-		return 0, err
-	}
+    addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+    if err != nil {
+        return 0, err
+    }
 
-	l, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		return 0, err
-	}
-	defer func() {
-		_ = l.Close()
-	}()
-	return l.Addr().(*net.TCPAddr).Port, nil
+    l, err := net.ListenTCP("tcp", addr)
+    if err != nil {
+        return 0, err
+    }
+    defer func() {
+        _ = l.Close()
+    }()
+    return l.Addr().(*net.TCPAddr).Port, nil
+}
+
+// GetLocalIP returns the non loopback local IP of the host
+func GetLocalIP() string {
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        return ""
+    }
+    for _, address := range addrs {
+        // check the address type and if it is not a loopback the display it
+        if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+            if ipnet.IP.To4() != nil {
+                return ipnet.IP.String()
+            }
+        }
+    }
+    return ""
+}
+
+// GetOutboundIP Get preferred outbound ip of this machine
+func GetOutboundIP() (string, error) {
+    conn, err := net.Dial("udp", "8.8.8.8:80")
+    if err != nil {
+        return "", err
+    }
+    defer conn.Close()
+
+    localAddr := conn.LocalAddr().(*net.UDPAddr)
+    return localAddr.IP.To4().String(), nil
+}
+
+func GetManagementIp() string {
+
+    return os.Getenv("MANAGEMENT_IP")
+}
+
+func GetClientIp() string {
+
+    return os.Getenv("CLIENT_IP")
+}
+
+func GetHostIp() string {
+
+    return os.Getenv("HOST_IP")
 }
