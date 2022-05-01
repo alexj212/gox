1d02f1e7c080cfcf8ccfa5316e89017f92b7ea28
Alex Jeannopoulos <alex@jeannopoulos.com>
2022-05-01 13:31:13 -0400

    latest working - .999 release

--- app.go
+++ app.go
@@ -1,116 +1,182 @@
 package gox
 
 import (
-	"fmt"
-	"github.com/droundy/goopt"
-	"io/ioutil"
-	"log"
-	"os"
-	"path/filepath"
-	"strings"
+    "crypto/rand"
+    "crypto/rsa"
+    "crypto/x509"
+    "encoding/pem"
+    "fmt"
+    "github.com/droundy/goopt"
+    "github.com/pkg/errors"
+    "io/ioutil"
+    "log"
+    "os"
+    "path/filepath"
+    "strings"
 )
 
 var (
-	showHistory  = goopt.Flag([]string{"-H"}, nil, "Show history", "")
-	clearHistory = goopt.Flag([]string{"--XH"}, nil, "Clear history", "")
+    showHistory  = goopt.Flag([]string{"-H"}, nil, "Show history", "")
+    clearHistory = goopt.Flag([]string{"--XH"}, nil, "Clear history", "")
 )
 
 // HandleHistory will save the command line history to file in the home dir. If no arguments are passed to the app, it will
 // display history of past commands executed. If an error occurs it will be returned.
 func HandleHistory() (exitApp bool, err error) {
 
-	if *clearHistory {
-		historyFileName := GetHistoryFileName()
-		fmt.Printf("Removing history file %s\n", historyFileName)
-		os.Remove(historyFileName)
-	}
+    if *clearHistory {
+        historyFileName := GetHistoryFileName()
+        fmt.Printf("Removing history file %s\n", historyFileName)
+        os.Remove(historyFileName)
+    }
 
-	if *showHistory {
-		historyBytes, err := GetHistoryFile()
-		if err != nil {
-			return true, err
-		}
+    if *showHistory {
+        historyBytes, err := GetHistoryFile()
+        if err != nil {
+            return true, err
+        }
 
-		if len(historyBytes) == 0 {
-			usage := goopt.Usage()
-			fmt.Printf("Command has never been run\nUsage:\n%s\n", usage)
-			return true, nil
-		}
-		fmt.Printf("Showing history for command: %s\n", GetAppName())
-		fmt.Printf("\n%s\b", string(historyBytes))
-		return true, nil
-	}
+        if len(historyBytes) == 0 {
+            usage := goopt.Usage()
+            fmt.Printf("Command has never been run\nUsage:\n%s\n", usage)
+            return true, nil
+        }
+        fmt.Printf("Showing history for command: %s\n", GetAppName())
+        fmt.Printf("\n%s\b", string(historyBytes))
+        return true, nil
+    }
 
-	appendHistory()
-	return false, nil
+    appendHistory()
+    return false, nil
 }
 
 // GetAppName return name of executable sans path
 func GetAppName() (appName string) {
-	_, appName = filepath.Split(os.Args[0])
-	return
+    _, appName = filepath.Split(os.Args[0])
+    return
 }
 
 // GetHistoryFileName return name of history file
 func GetHistoryFileName() (historyFileName string) {
-	dirname, _ := os.UserHomeDir()
-	historyFileName = filepath.Join(dirname, fmt.Sprintf(".%s.history", GetAppName()))
-	return
+    dirname, _ := os.UserHomeDir()
+    historyFileName = filepath.Join(dirname, fmt.Sprintf(".%s.history", GetAppName()))
+    return
 }
 
 // GetHistoryFile read history file to []byte.
 func GetHistoryFile() ([]byte, error) {
-	historyFileName := GetHistoryFileName()
-	exists := FileExists(historyFileName)
-	if exists {
-		f, err := ioutil.ReadFile(historyFileName)
-		if err != nil {
-			return nil, err
-		}
-		return f, err
-	}
+    historyFileName := GetHistoryFileName()
+    exists := FileExists(historyFileName)
+    if exists {
+        f, err := ioutil.ReadFile(historyFileName)
+        if err != nil {
+            return nil, err
+        }
+        return f, err
+    }
 
-	payload := make([]byte, 0)
-	ioutil.WriteFile(historyFileName, payload, 0644)
-	return payload, nil
+    payload := make([]byte, 0)
+    ioutil.WriteFile(historyFileName, payload, 0644)
+    return payload, nil
 }
 
 func appendHistory() {
-	historyFileName := GetHistoryFileName()
-	var fullCmdLine string
-	if strings.Contains(os.Args[0], "go-build") {
-		fullCmdLine = "go run . "
-		fullCmdLine = fullCmdLine + strings.Join(os.Args[1:], " ")
-	} else {
-		fullCmdLine = strings.Join(os.Args, " ")
-	}
+    historyFileName := GetHistoryFileName()
+    var fullCmdLine string
+    if strings.Contains(os.Args[0], "go-build") {
+        fullCmdLine = "go run . "
+        fullCmdLine = fullCmdLine + strings.Join(os.Args[1:], " ")
+    } else {
+        fullCmdLine = strings.Join(os.Args, " ")
+    }
 
-	historyBytes, err := GetHistoryFile()
-	if err != nil {
-		return
-	}
+    historyBytes, err := GetHistoryFile()
+    if err != nil {
+        return
+    }
 
-	history := string(historyBytes)
-	if strings.Contains(history, fullCmdLine) {
-		log.Printf("cmd line: %s already in history file\n", fullCmdLine)
-		return
-	}
+    history := string(historyBytes)
+    if strings.Contains(history, fullCmdLine) {
+        log.Printf("cmd line: %s already in history file\n", fullCmdLine)
+        return
+    }
 
-	f, err := os.OpenFile(historyFileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
-	if err != nil {
-		log.Printf("Error opening history file: %s error: %v\n", historyFileName, err)
-		return
-	}
+    f, err := os.OpenFile(historyFileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+    if err != nil {
+        log.Printf("Error opening history file: %s error: %v\n", historyFileName, err)
+        return
+    }
 
-	defer f.Close()
+    defer f.Close()
 
-	if _, err = f.WriteString(fullCmdLine + "\n"); err != nil {
-		log.Printf("Error writing to  history file: %s error: %v\n", historyFileName, err)
-	}
+    if _, err = f.WriteString(fullCmdLine + "\n"); err != nil {
+        log.Printf("Error writing to  history file: %s error: %v\n", historyFileName, err)
+    }
 }
 
 // GetDefaultLogDir return default log dir for the app
 func GetDefaultLogDir() (logDir string) {
-	logDir = filepath.Join(os.TempDir(), GetAppName())
-	return
+    logDir = filepath.Join(os.TempDir(), GetAppName())
+    return
+}
+
+// GetAppKeyFileName return name of app key file
+func GetAppKeyFileName() (appKeyFileName string) {
+    dirname, _ := os.UserHomeDir()
+    appKeyFileName = filepath.Join(dirname, fmt.Sprintf(".%s.key", GetAppName()))
+    return
+}
+
+// GetAppKey read AppKey file to rsa.PrivateKey.
+func GetAppKey() (*rsa.PrivateKey, error) {
+    appKeyFileName := GetAppKeyFileName()
+    exists := FileExists(appKeyFileName)
+    if exists {
+        f, err := ioutil.ReadFile(appKeyFileName)
+        if err != nil {
+            return nil, err
+        }
+
+        block, _ := pem.Decode(f)
+        if block != nil {
+            key, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+            if err != nil {
+                return nil, err
+            }
+            return key, nil
+        }
+        return nil, errors.New("unable to extract private key")
+    }
+
+    key, err := rsa.GenerateKey(rand.Reader, 2048)
+    if err != nil {
+        return nil, err
+    }
+    // Validate Private Key
+    err = key.Validate()
+    if err != nil {
+        return nil, err
+    }
+    log.Println("Private Key generated")
+    payload := encodePrivateKeyToPEM(key)
+    ioutil.WriteFile(appKeyFileName, payload, 0644)
+    return key, nil
+}
+
+// encodePrivateKeyToPEM encodes Private Key from RSA to PEM format
+func encodePrivateKeyToPEM(privateKey *rsa.PrivateKey) []byte {
+    // Get ASN.1 DER format
+    privDER := x509.MarshalPKCS1PrivateKey(privateKey)
+
+    // pem.Block
+    privBlock := pem.Block{
+        Type:    "RSA PRIVATE KEY",
+        Headers: nil,
+        Bytes:   privDER,
+    }
+
+    // Private key in PEM format
+    privatePEM := pem.EncodeToMemory(&privBlock)
+
+    return privatePEM
 }
---
+++ ssh.go
@@ -0,0 +1,98 @@
+package gox
+
+import (
+    "crypto/rand"
+    "crypto/rsa"
+    "crypto/x509"
+    "encoding/pem"
+    "fmt"
+    "io/ioutil"
+    "log"
+
+    "golang.org/x/crypto/ssh"
+)
+
+// LoadAuthorizedKeys load keys from authorized_keys file and puts the marshalled public key into the map as the key.
+func LoadAuthorizedKeys(authorizedKeyFile string) (map[string]bool, error) {
+    authorizedKeysBytes, err := ioutil.ReadFile(authorizedKeyFile)
+    if err != nil {
+        log.Printf("Failed to load authorized_keys, err: %v", err)
+        return nil, err
+    }
+
+    authorizedKeysMap := map[string]bool{}
+    for len(authorizedKeysBytes) > 0 {
+        pubKey, _, _, rest, err := ssh.ParseAuthorizedKey(authorizedKeysBytes)
+        if err != nil {
+            log.Printf("tes1: %v\n", err)
+            return nil, err
+        }
+
+        authorizedKeysMap[string(pubKey.Marshal())] = true
+        authorizedKeysBytes = rest
+        fmt.Printf("authorizedKeysMap add key type: %v\n", pubKey.Type())
+    }
+
+    return authorizedKeysMap, err
+}
+
+// GeneratePrivateKey creates a RSA Private Key of specified byte size
+func GeneratePrivateKey(bitSize int) (*rsa.PrivateKey, error) {
+    // Private Key generation
+    privateKey, err := rsa.GenerateKey(rand.Reader, bitSize)
+    if err != nil {
+        return nil, err
+    }
+
+    // Validate Private Key
+    err = privateKey.Validate()
+    if err != nil {
+        return nil, err
+    }
+
+    log.Println("Private Key generated")
+    return privateKey, nil
+}
+
+// EncodePrivateKeyToPEM encodes Private Key from RSA to PEM format
+func EncodePrivateKeyToPEM(privateKey *rsa.PrivateKey) []byte {
+    // Get ASN.1 DER format
+    privDER := x509.MarshalPKCS1PrivateKey(privateKey)
+
+    // pem.Block
+    privBlock := pem.Block{
+        Type:    "RSA PRIVATE KEY",
+        Headers: nil,
+        Bytes:   privDER,
+    }
+
+    // Private key in PEM format
+    privatePEM := pem.EncodeToMemory(&privBlock)
+
+    return privatePEM
+}
+
+// GeneratePublicKey take a rsa.PublicKey and return bytes suitable for writing to .pub file
+// returns in the format "ssh-rsa ..."
+func GeneratePublicKey(privatekey *rsa.PublicKey) ([]byte, error) {
+    publicRsaKey, err := ssh.NewPublicKey(privatekey)
+    if err != nil {
+        return nil, err
+    }
+
+    pubKeyBytes := ssh.MarshalAuthorizedKey(publicRsaKey)
+
+    log.Println("Public key generated")
+    return pubKeyBytes, nil
+}
+
+// WriteKeyToFile writes keys to a file
+func WriteKeyToFile(keyBytes []byte, saveFileTo string) error {
+    err := ioutil.WriteFile(saveFileTo, keyBytes, 0600)
+    if err != nil {
+        return err
+    }
+
+    log.Printf("Key saved to: %s", saveFileTo)
+    return nil
+}
