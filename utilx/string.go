3dd29ee674bcabaec067f125c84d83d2ab2da40a
Alex Jeannopoulos <alex@jeannopoulos.com>
2023-01-31 17:03:41 -0500

    latest version: v0.2.3

--- utilx/string.go
+++ utilx/string.go
@@ -6,10 +6,10 @@ import (
 )
 
 // ToString util to convert obj to json string
-func ToString(obj interface{}) (jsonStr string) {
+func ToString(obj interface{}) string {
 	b, err := json.MarshalIndent(obj, "", "")
 	if err != nil {
 		return fmt.Sprintf("obj: %T %v could not marshal to json err: %v", obj, obj, err)
 	}
-	return b
+	return string(b)
 }
